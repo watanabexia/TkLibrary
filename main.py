a5f04e632dfb7f29d51ca8cfb64bff5140de2952
Tongguang Watanabe Xia <ghbq674521@163.com>
2022-02-18 23:42:14 +0800

    add basic components for reservation

--- main.py
+++ main.py
@@ -33,7 +33,11 @@ import tkinter as tk
 # UI Initialization
 root = tk.Tk()
 root.title('ALS')
-root.geometry('1000x1000')
+
+win_w = 1000
+win_h = 1000
+
+root.geometry(str(win_w) + "x" + str(win_h))
 root.option_add("*font", "SF\ Pro 14")
 
 # Frame Definition
@@ -41,7 +45,7 @@ Root_frame = tk.Frame(root)
 Mem_frame = tk.Frame(root)
 Book_frame = tk.Frame(root)
 Loan_frame = tk.Frame(root)
-Res_frame = tk.Frame(root)
+Res_frame = tk.Frame(root, height = win_h, width = win_w)
 Fine_frame = tk.Frame(root)
 Rep_frame = tk.Frame(root)
 
@@ -63,6 +67,80 @@ Fine_button = tk.Button(Root_frame, text = "Fines", fg = 'black', command = lamb
 Fine_button.pack()
 Rep_button = tk.Button(Root_frame, text = "Reports", fg = 'black', command = lambda: change_frame(Root_frame, Rep_frame))
 Rep_button.pack()
+
+# Qingyang begins
+# Reservation frame object
+Res_book_frame = tk.Frame(root, height = win_h, width = win_w)
+Res_cancel_frame = tk.Frame(root, height = win_h, width = win_w)
+
+Res_book_label = tk.Label(Res_frame, text = "Book Reservation", fg = 'black')
+Res_book_label.place(x = 50, y = 50, anchor = "nw")
+Res_book_button = tk.Button(Res_frame, text = "Reserve a Book", fg = 'black', command = lambda: change_frame(Res_frame, Res_book_frame))
+Res_book_button.place(x = 300, y = 50, anchor = "nw")
+Res_cancel_label = tk.Label(Res_frame, text = "Reservation Cancellation", fg = 'black')
+Res_cancel_label.place(x = 50, y = 100, anchor = "nw")
+Res_cancel_button = tk.Button(Res_frame, text = "Cancel Reservation", fg = 'black', command = lambda: change_frame(Res_frame, Res_cancel_frame))
+Res_cancel_button.place(x = 300, y = 100, anchor = "nw")
+
+Res_book_title_label = tk.Label(Res_book_frame, text = "To reserve a book, please enter information below:", fg = 'black')
+Res_book_title_label.place(x = 50, y = 0, anchor = "nw")
+Res_book_Acc_number_label = tk.Label(Res_book_frame, text = "Accession Number", fg = 'black')
+Res_book_Acc_number_label.place(x = 50, y = 50, anchor = "nw")
+Res_book_Acc_number_entry = tk.Entry(Res_book_frame, fg = 'black', width = 60)
+Res_book_Acc_number_entry.insert(0, "Used to identify an instance of book")
+Res_book_Acc_number_entry.place(x = 300, y = 50, anchor = "nw")
+Res_book_Mem_ID_label = tk.Label(Res_book_frame, text = "Membership ID", fg = 'black')
+Res_book_Mem_ID_label.place(x = 50, y = 100, anchor = "nw")
+Res_book_Mem_ID_entry = tk.Entry(Res_book_frame, fg = 'black', width = 60)
+Res_book_Mem_ID_entry.insert(0, "A unique alphanumeric id that distinguishes every member")
+Res_book_Mem_ID_entry.place(x = 300, y = 100, anchor = "nw")
+Res_book_Res_date_label = tk.Label(Res_book_frame, text = "Reserve date", fg = 'black')
+Res_book_Res_date_label.place(x = 50, y = 150, anchor = "nw")
+Res_book_Res_date_entry = tk.Entry(Res_book_frame, fg = 'black', width = 60)
+Res_book_Res_date_entry.insert(0, "Date of book reservation")
+Res_book_Res_date_entry.place(x = 300, y = 150, anchor = "nw")
+
+Res_book_Res_button = tk.Button(Res_book_frame, text = "Reserve Book", fg = 'black')
+Res_book_Res_button.place(x = 50, y = 200, anchor = "nw")
+Res_book_Back_button = tk.Button(Res_book_frame, text = "Back to Reservation Menu", fg = 'black', command = lambda: change_frame(Res_book_frame, Res_frame))
+Res_book_Back_button.place(x = 700, y = 200, anchor = "nw")
+
+def confirm_book_reservation():
+    acc_number = Res_book_Acc_number_entry.get()
+
+    book_title = ""
+
+    mem_id = Res_book_Mem_ID_entry.get()
+
+
+Res_cancel_title_label = tk.Label(Res_cancel_frame, text = "To cancel a reservation, please enter information below:", fg = 'black')
+Res_cancel_title_label.place(x = 50, y = 0, anchor = "nw")
+Res_cancel_Acc_number_label = tk.Label(Res_cancel_frame, text = "Accession Number", fg = 'black')
+Res_cancel_Acc_number_label.place(x = 50, y = 50, anchor = "nw")
+Res_cancel_Acc_number_entry = tk.Entry(Res_cancel_frame, fg = 'black', width = 60)
+Res_cancel_Acc_number_entry.insert(0, "Used to identify an instance of book")
+Res_cancel_Acc_number_entry.place(x = 300, y = 50, anchor = "nw")
+Res_cancel_Mem_ID_label = tk.Label(Res_cancel_frame, text = "Membership ID", fg = 'black')
+Res_cancel_Mem_ID_label.place(x = 50, y = 100, anchor = "nw")
+Res_cancel_Mem_ID_entry = tk.Entry(Res_cancel_frame, fg = 'black', width = 60)
+Res_cancel_Mem_ID_entry.insert(0, "A unique alphanumeric id that distinguishes every member")
+Res_cancel_Mem_ID_entry.place(x = 300, y = 100, anchor = "nw")
+Res_cancel_Cancel_date_label = tk.Label(Res_cancel_frame, text = "Cancel date", fg = 'black')
+Res_cancel_Cancel_date_label.place(x = 50, y = 150, anchor = "nw")
+Res_cancel_Cancel_date_entry = tk.Entry(Res_cancel_frame, fg = 'black', width = 60)
+Res_cancel_Cancel_date_entry.insert(0, "Date of reservation cancellation")
+Res_cancel_Cancel_date_entry.place(x = 300, y = 150, anchor = "nw")
+
+Res_cancel_Res_button = tk.Button(Res_cancel_frame, text = "Cancel Reservation", fg = 'black')
+Res_cancel_Res_button.place(x = 50, y = 200, anchor = "nw")
+Res_cancel_Back_button = tk.Button(Res_cancel_frame, text = "Back to Reservation Menu", fg = 'black', command = lambda: change_frame(Res_cancel_frame, Res_frame))
+Res_cancel_Back_button.place(x = 700, y = 200, anchor = "nw")
+
+# Qingyang ends
+
+
+
+
 
 # Root Frame Application
 Root_frame.pack()
